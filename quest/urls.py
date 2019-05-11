abaa6e8cc5573088745a2e428c67e3b242e70b10
Diego Costa <diegocostacmp@gmail.com>
2019-05-11 09:36:45 -0300

    Autenticacao com signIn e SignUp com Firebase

--- quest/urls.py
+++ quest/urls.py
@@ -6,5 +6,10 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.signIn),
-    path('postsign/', views.postsign)
+    path('postsign/', views.postsign),
+    path('logout/', views.logout, name='logout'),
+    path('signup/', views.signup, name='signup'),
+    path('postsignup/', views.postsignup, name='postsignup'),
+    path('create/', views.create, name='create')
+
 ]
--- quest/views.py
+++ quest/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 import pyrebase
 
+from django.contrib import auth
+
 config = {
     'apiKey':               "AIzaSyDbScV081zr7iWPEmiCN9kPi3jeZlgWmTk",
     'authDomain':           "e-quest-aaecf.firebaseapp.com",
@@ -11,15 +13,71 @@ config = {
 }
 firebase = pyrebase.initialize_app(config)
 
-auth = firebase.auth()
+authe = firebase.auth()
+database = firebase.database()
 
 def signIn(request):
     return render(request, 'signIn.html')
 
+# autenticacao via firebase
 def postsign(request):
     email = request.POST.get('email')
     passw = request.POST.get('pass')
 
-    user = auth.sign_in_with_email_and_password(email, passw)
+    try:
+        user = authe.sign_in_with_email_and_password(email, passw)
+    except:
+        message = 'Credenciais Invalidas!'
+        return render(request, 'signIn.html', {'msg': message})
+    print('Token de acesso: \n', user['idToken'])
+    session_id=user['idToken']  
+    request.session['uid']=str(session_id)  
 
     return render(request, 'welcome.html', {'e':email})
+
+def logout(request):
+    auth.logout(request)
+    return render(request, 'signIn.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def postsignup(request):
+    name = request.POST.get('name')
+    email = request.POST.get('email')
+    passw = request.POST.get('pass')
+
+    print(email)
+    print(passw)
+    try:
+        user=authe.create_user_with_email_and_password(email,passw)
+        uid = user['localId']
+        data={"name":name, "status":"1"}
+        database.child("users").child(uid).child("details").set(data)
+    except:
+        message='Nao foi possivel criar a conta!'
+        return render(request, 'signup.html', {'msg':message})
+    
+    return render(request, 'signIn.html')
+
+def create(request):
+    
+    return render(request, 'create.html')
+
+def post_create(request):
+    import time 
+    from datetime import datetime, timezone
+    import pytz
+
+    tz = pytz.timezone('Asia/Kolkata')
+    time_now = datetime.now(timezone.utc).astimezone(tz)
+    millis = int(time.mktime(time_now.timetuple()))
+
+    work = request.POST.get('work')
+    progress = request.POST.get('progress')
+
+    data = {
+        'work': work,
+        'progress': progress
+    }
+    return render(request, 'welcome.html')
