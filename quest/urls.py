9100f3ebe929e74c36ea61ab1595e39baf40e563
Diego Costa <diegocostacmp@gmail.com>
2019-06-24 21:29:19 -0300

    Adicionando apps quiz, game e relatorio

--- quest/urls.py
+++ quest/urls.py
@@ -7,6 +7,9 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 # redireciomanento para os apps
 urlpatterns = [
     path('', include('apps.core.urls', namespace='core')),
+    path('quiz/', include('apps.quiz.urls', namespace='quiz')),
+    path('game/', include('apps.game.urls', namespace='game')),
+    path('relatorio/', include('apps.relatorio.urls', namespace='relatorio')),
     path('admin/', admin.site.urls)
 
 ]   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
