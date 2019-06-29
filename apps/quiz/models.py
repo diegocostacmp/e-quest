a04d149f7e767c3f36f002f235d1a9b4eb4a8e1f
Diego Costa <diegocostacmp@gmail.com>
2019-06-29 10:46:51 -0300

    Adicionando status personalizado para disciplina

--- apps/quiz/models.py
+++ apps/quiz/models.py
@@ -5,15 +5,26 @@ from django.core.validators import MinValueValidator, MinLengthValidator
 from django.db import models
 from django.urls import reverse
 
+from django.utils.safestring import mark_safe
+
 from apps.core.models import Usuario
+
+# Status choices
+STATUS_CHOICES = (
+        ("A", "Ativo"),
+        ("B", "Bloqueado"),
+        ("D", "Desativado")
+    )
 
 # Cadastro de disciplina
 class Disciplina(models.Model):
+
     titulo          = models.CharField(verbose_name="Título", max_length=128, help_text="Digite o nome da disciplina", null=False, blank=False, default=None)
     descricao       = models.CharField(verbose_name="Descrição", max_length=512, help_text="Digite a descrição da disciplina", null=True, blank=True, default=None)
     uuid            = models.UUIDField(verbose_name='Identificador Único', default=uuid.uuid4, editable=False)
     data_criacao    = models.DateTimeField(verbose_name="Data criação", auto_now_add=True, blank=True)
     data_alteracao  = models.DateTimeField(verbose_name="Data alteração", auto_now_add=True, blank=True)
+    status          = models.CharField(choices=STATUS_CHOICES, max_length=15, default="A")
 
     # fks
     professor       = models.ForeignKey(Usuario, on_delete=models.CASCADE)
@@ -23,4 +34,12 @@ class Disciplina(models.Model):
 
     def get_professor(self):
         return self.professor.nome_completo
+
+    def get_status(self):
+        if self.status == 'A':
+            return mark_safe('<span class="kt-badge  kt-badge--success kt-badge--inline kt-badge--pill">Ativo</span>') 
+        elif self.status == 'B':
+            return mark_safe('<span class="kt-badge  kt-badge--danger kt-badge--inline kt-badge--pill">Bloqueado</span>') 
+        else:
+            return mark_safe('<span class="kt-badge  kt-badge--warning kt-badge--inline kt-badge--pill">Desativo</span>') 
 
--- apps/quiz/tables.py
+++ apps/quiz/tables.py
@@ -3,9 +3,16 @@ from .models import Disciplina
 
 
 class DisciplinaTable(tables.Table):
+    # Acessor para obter nome do professor
     professor= tables.Column(accessor='get_professor')
+
+    # Template com as acoes disponiveis
+    acoes = tables.TemplateColumn(template_name='disciplina/disciplina_actions.html')
+
+    # Status personalizado
+    status = tables.Column(accessor='get_status')
     class Meta:
         model = Disciplina
-        sequence = ['titulo', 'descricao', 'professor']
+        sequence = ['titulo', 'descricao', 'professor', 'status', 'acoes']
         exclude = ['id', 'uuid', 'data_criacao', 'data_alteracao']
       
